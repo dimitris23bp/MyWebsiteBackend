4c50adbd27f9a997cb117a57b8015f6e1460c55a
Dimitris <dimitrios.karamanis@telekom.com>
2023-11-04 19:34:07 +0200

    Initial implementation of Github controller

--- src/Controllers/GithubController.cs
+++ src/Controllers/GithubController.cs
@@ -1,14 +1,41 @@
 using Microsoft.AspNetCore.Mvc;
+using Octokit;
 
-namespace MyWebsiteBackend.Controllers;
-
-[ApiController]
-[Route("[controller]")]
-public class GithubController : ControllerBase
+namespace MyWebsiteBackend.Controllers
 {
-    [HttpGet(Name = "GetGithub")]
-    public string Get()
+    [ApiController]
+    [Route("[controller]")]
+    public class GithubController : ControllerBase
     {
-        return "Hello World!";
+        private readonly GitHubClient Client;
+        private readonly string Username = "dimitris23bp";
+
+        public GithubController()
+        {
+            Client = new GitHubClient(new ProductHeaderValue(Username))
+            {
+                Credentials = new Credentials(Environment.GetEnvironmentVariable("TOKEN_GITHUB"))
+            };
+        }
+
+        [HttpGet("repositories")]
+        public async Task<IActionResult> GetGitHubRepo()
+        {
+            var request = new SearchRepositoriesRequest()
+            {
+                User = Username
+            };
+            var response = await Client.Search.SearchRepo(request);
+
+            return Ok(response);
+        }
+
+        [HttpGet("repository/{repoName}")]
+        public async Task<IActionResult> GetGitHubRepo(string repoName)
+        {
+            var response = await Client.Repository.Get(Username, repoName);
+
+            return Ok(response);
+        }
     }
 }
